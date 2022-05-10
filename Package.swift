d093e36385eaa7d4404cf502e6ea0764b5aec1df
wlgemini <wangluguang@live.com>
2022-05-10 11:37:21 +0800

    init spm

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Far",
+    
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)],
+    
+    products: [
+        .library(name: "Far", targets: ["Far"])
+    ],
+    
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0"))
+    ],
+    
+    targets: [
+        .target(name: "Far",
+                dependencies: ["Alamofire"],
+                path: "Sources",
+                linkerSettings: [.linkedFramework("Foundation", .when(platforms: [.iOS, .macOS, .tvOS, .watchOS]))]),
+        
+        .testTarget(name: "FarTests",
+                    dependencies: ["Far", "Alamofire"],
+                    path: "Tests")
+    ]
+)
---
+++ Sources/Far.swift
@@ -0,0 +1,6 @@
+public struct Far {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
